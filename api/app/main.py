de72bb77c65c2004b7a37a775155b1f770d629b5
jpsarodrigues@gmail.com <jpsarodrigues@gmail.com>
2021-05-19 21:57:22 +0100

    Changed database to pandas df.

--- api/app/main.py
+++ api/app/main.py
@@ -2,30 +2,30 @@ from typing import Optional
 
 from fastapi import FastAPI, HTTPException
 
-import sqlite3
+import pandas as pd
 
 app = FastAPI()
 
 @app.on_event('startup')
 def startup():
-    global db_conn, cur
+    global df
 
-    db_path = 'db/cod_post_freg_matched.db'
-    db_conn = sqlite3.connect(db_path, check_same_thread=False)
-    cur = db_conn.cursor()
-
-@app.on_event("shutdown")
-def shutdown():
-    db_conn.disconnect()
+    url = 'https://raw.githubusercontent.com/dssg-pt/mp-mapeamento-cp7/main/output_data/cod_post_freg_matched.csv'
+    df = pd.read_csv(url, index_col='CodigoPostal')
 
 @app.get("/{cod_postal}")
 def read_item(cod_postal: int):
-    db_entry = cur.execute(f'''SELECT CodigoPostal, Distrito, Concelho, Freguesia FROM cod_post_freg
-                              WHERE CodigoPostal = {cod_postal}''').fetchall()
-    if not len(db_entry):
-        raise HTTPException(status_code=404, detail='Codigo postal não encontrado na base de dados.')
-    elif None in db_entry[0]:
-        raise HTTPException(status_code=404, detail='Informação sobre código postal em falta.')
+    keys = ['Distrito', 'Concelho', 'Freguesia']
 
-    keys = ['CodigoPostal', 'Distrito', 'Concelho', 'Freguesia']
-    return {0: {key: val for key, val in zip(keys, db_entry[0])}}
+    try:
+        df_entry = df.loc[cod_postal, keys]
+    except KeyError:
+        raise HTTPException(status_code=404, detail='Codigo postal não encontrado na base de dados.')
+
+    if df_entry.isna().any():
+        raise HTTPException(status_code=404, detail='Informação sobre código postal em falta.')
+    
+    df_entry_dict = df_entry.to_dict()
+    df_entry_dict['CodigoPostal'] = cod_postal
+
+    return {0: df_entry_dict}
